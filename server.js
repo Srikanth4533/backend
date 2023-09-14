74ffc6a1b002ca3a637457925b37617c95ff8e63
Srikanth4533 <kondakallasrikanth991@gmail.com>
2023-09-14 10:55:52 +0530

    added few routes

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
+app.get("/sri", (req, res) => {
+  res.send("Hello Srikanth");
+});
+
 app.listen(4000, () => {
   console.log(`Server started at port no 4000`);
 });
